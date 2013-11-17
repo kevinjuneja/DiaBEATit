1a3d5e57d9684e3c94c2889dae365ffeb0780989
Kevin Juneja <kevin.j.24@gmail.com>
2013-11-16 23:36:14 -0800

    added placeholder splashscreen view

---
+++ DiaBEATit/SplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreenViewController.h
+//  DiaBEATit
+//
+//  Created by Kevin Juneja on 11/16/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreenViewController : UIViewController
+
+@end
---
+++ DiaBEATit/SplashScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  SplashScreenViewController.m
+//  DiaBEATit
+//
+//  Created by Kevin Juneja on 11/16/13.
+//  Copyright (c) 2013 App Jam. All rights reserved.
+//
+
+#import "SplashScreenViewController.h"
+
+@interface SplashScreenViewController ()
+
+@end
+
+@implementation SplashScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
